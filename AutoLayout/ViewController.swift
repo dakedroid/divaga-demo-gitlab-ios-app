748c1fb3824aec166b3e36507ef52d165e7230cf
dakedroid <dave1998._@hotmail.com>
2018-10-22 14:33:07 -0500

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Kevin Molina on 22/10/18.
+//  Copyright © 2018 Kevin Molina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
